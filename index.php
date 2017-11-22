17a24de0c98d3df2b18edf2b2b62d0cfb4f38203
Raphaël PEYTAVIN DE GARAM <r.peytavin@DIGINAMIC.LOCAL>
2017-11-22 17:42:37 +0100

    raph commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<h2> Calcul renouvellement période d'essai</h2>
+
+<?PHP
+/* Anthony, Dylan,
+  Arretez de vous br**** les c***** et travailler un peu !!!!!!!
+  Cordialement
+  Raphael
+ */
+
+$nom;
+$prenom;
+$date_debut_contrat;
+$id_cdi;
+
+
+function getRenouvellementPE($date_debut_contrat){
+  return null;
+}
+?>
